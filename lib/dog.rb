1497b5ec092111963fc156b81fb9bdb83f844eac
Stiflerzak <hilarymwendia97@gmail.com>
2023-02-11 16:55:57 +0300

    done With assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
+    #setter method
+def name=(dog_name)
+    #assign the @name instance variable a value of whatever is passing as an argument
+    @this_dogs_name = dog_name
 end
+
+#getter method
+def name
+    #access the data from the @name instance variable and return it
+    @this_dogs_name
+    end
+end
+
+lassie = Dog.new
+
+lassie.name= "Lassie"
+
+puts lassie.name
